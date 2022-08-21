5025dfcdfea82823d9a96fc99006d67628bc2859
Elias Gill <eliasgill42@gmail.com>
2022-08-21 17:54:46 -0400

    basura

--- tarea2/busqueda/src/busqueda/Busqueda.java
+++ tarea2/busqueda/src/busqueda/Busqueda.java
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package busqueda;
-
-import static javafx.scene.input.KeyCode.M;
 
 /**
  *
@@ -18,145 +15,43 @@ public class Busqueda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 20) {
-            int N = i * 50000;
-            
+        // Hasta donde iterar
+        final int constante = 50000;
+        double t1, t2;
+        double b1, b2;
+        for (int N = 50000; N <= constante; N += 50000) {
             double media1 = 0, media2 = 0;
-            
-                double vector[] = new double[N];
-                for (int x = 0; x < vector.length; x++) {
-                    double valorEntero = (Math.floor(Math.random() * (1000000 - 10000 + 1) + 10000));// Valor entre M y N, ambos incluidos.
-                    vector[x] = valorEntero;
+
+            // repetir N veces otra vez
+            for (int i = 0; i < N; i++) {
+                // generar numeros aleatorios
+                Double vector[] = new Double[N];
+                for (int j = 0; j < vector.length; j++) {
+                    double valorEntero = (Math.floor(Math.random() * (1000000 - 10000 + 1) +
+                            10000));
+                    vector[j] = valorEntero;
                 }
+
+                // Elemento aleatorio del vector
                 int elemento = (int) (Math.floor(Math.random() * (N - 1)));
-                double valor = vector[elemento];
-                double t1, t2;//Para la busqueda Lineal
+                // Busqueda Lineal
                 t1 = System.currentTimeMillis();
-                buscar.lineal(vector, valor);
+                buscar.lineal(vector, vector[elemento]);
                 t2 = System.currentTimeMillis();
-                media1 = t2-t1;
+                media1 += t2 - t1;
 
-                // Para la busqueda Binaria
-                quickSort(vector);
-                double b1, b2;
+                // Busqueda Binaria
+                Util.quicksort(vector);
                 b1 = System.currentTimeMillis();
-                buscar.busquedabinaria(vector, valor);
+                buscar.busquedaBinaria(vector, vector[elemento]);
                 b2 = System.currentTimeMillis();
-                media2 = (b2 - b1);
-            
+                media2 += b2 - b1;
+            }
+            // imprimir resultados parciales
             System.out.printf("%10d | %10f | %10f | %10f | %10f | %10f | %10f | %10f | %10f\n",
-                    N, media1, media1 / N, media1 / (N * (Math.log(media1) / Math.log(2))), media1 / (N * N),
-                    media2, media2 / N, media2 / (N * (Math.log(media2) / Math.log(2))), media2 / (N * N));
-            
-            i++;
-        }
-
-    }
-
-    public static void quickSort(double[] intArray) {
-        recQuickSort(intArray, 0, intArray.length - 1);
-    }
-
-    public static void recQuickSort(double[] intArray, int left, int right) {
-        int size = right - left + 1;
-        if (size <= 3) {
-            manualSort(intArray, left, right);
-        } else {
-            double median = medianOf3(intArray, left, right);
-            int partition = partitionIt(intArray, left, right, median);
-            recQuickSort(intArray, left, partition - 1);
-            recQuickSort(intArray, partition + 1, right);
-        }
-    }
-
-    public static double medianOf3(double[] intArray, int left, int right) {
-        int center = (left + right) / 2;
-
-        if (intArray[left] > intArray[center]) {
-            swap(intArray, left, center);
-        }
-
-        if (intArray[left] > intArray[right]) {
-            swap(intArray, left, right);
-        }
-
-        if (intArray[center] > intArray[right]) {
-            swap(intArray, center, right);
-        }
-
-        swap(intArray, center, right - 1);
-        return intArray[right - 1];
-    }
-
-    public static void swap(double[] intArray, int dex1, int dex2) {
-        double temp = intArray[dex1];
-        intArray[dex1] = intArray[dex2];
-        intArray[dex2] = temp;
-    }
-
-    public static int partitionIt(double[] intArray, int left, int right, double pivot) {
-        int leftPtr = left;
-        int rightPtr = right - 1;
-
-        while (true) {
-            while (intArray[++leftPtr] < pivot)
-        ;
-            while (intArray[--rightPtr] > pivot)
-        ;
-            if (leftPtr >= rightPtr) {
-                break;
-            } else {
-                swap(intArray, leftPtr, rightPtr);
-            }
-        }
-        swap(intArray, leftPtr, right - 1);
-        return leftPtr;
-    }
-
-    public static void manualSort(double[] intArray, int left, int right) {
-        int size = right - left + 1;
-        if (size <= 1) {
-            return;
-        }
-        if (size == 2) {
-            if (intArray[left] > intArray[right]) {
-                swap(intArray, left, right);
-            }
-            return;
-        } else {
-            if (intArray[left] > intArray[right - 1]) {
-                swap(intArray, left, right - 1);
-            }
-            if (intArray[left] > intArray[right]) {
-                swap(intArray, left, right);
-            }
-            if (intArray[right - 1] > intArray[right]) {
-                swap(intArray, right - 1, right);
-            }
-        }
-    }
-
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package busqueda;
-
-/**
- *
- * @author Admin
- */
-public class Busqueda {
-    public static void main(String[] args) {
-        double vector[] = {1,978.1, 3.3, 9, 44, 2, 4, 5};
-        double aux[] = buscar.sort(vector);
-        for (double var : aux) {
-            System.out.println(var);
+                    N, media1, media1 / N, media1 / (N * (Math.log(N) / Math.log(2))),
+                    media1 / (N * N),
+                    media2, media2 / N, media2 / (N * (Math.log(N) / Math.log(2))), media2 / (N * N));
         }
     }
 }
->>>>>>> e3035f2 (no funciona fibbo)
--- tarea2/busqueda/src/busqueda/buscar.java
+++ tarea2/busqueda/src/busqueda/buscar.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,109 +9,29 @@ package busqueda;
  *
  * @author Admin
  */
-public class buscar {
-    public static void lineal(double vector[],double valor){
-        int i=0;
-        int posicion=-1;
-        while(i<vector.length){
-            if (vector[i]==valor){
-                posicion=i;
-            }
-            i++;
-        }
-    }
-   
-public static void busquedabinaria(double vector[],double valor){
-        int low_indice=0;
-        int high_indice=vector.length-1;
-        int posicion=-1;
-        while(low_indice<=high_indice){
-            int medio_indice=(low_indice+high_indice)/2;
-            if(valor==vector[medio_indice]){
-                posicion=medio_indice;
-                break;
-            }else if(valor<vector[medio_indice]){
-                high_indice=medio_indice-1;
-            }else if(valor>vector[medio_indice]){
-                low_indice=medio_indice+1;
-            }
-        }
-        
-    }
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package busqueda;
-
-/**
- *
- * @author Admin
- */
-public class buscar {
-    public static int lineal(double vector[], double valor) {
-        int i = 0;
-        while (i < vector.length) {
+public class buscar<T> {
+    public static <T> int lineal(T vector[], T valor) {
+        int posicion = -1;
+        for (int i = 0; i < vector.length; i++) {
             if (vector[i] == valor) {
                 return i;
             }
-            i++;
+        }
+        return posicion;
+    }
+
+    public static <T extends Comparable<T>> int busquedaBinaria(T[] vector, T valor) {
+        int inicio = 0, fin = vector.length - 1, posicion;
+        while (inicio <= fin) {
+            posicion = (inicio + fin) / 2;
+            if (vector[posicion].compareTo(valor) < 0) {
+                inicio = posicion + 1;
+            } else if (vector[posicion].compareTo(valor) > 0) {
+                fin = posicion - 1;
+            } else {
+                return posicion;
+            }
         }
         return -1;
     }
-
-    public static double[] sort(double vector[]) {
-        double[] vect = quicksort(vector, 0, vector.length - 1);
-        return vect;
-    }
-
-    private static double[] quicksort(double vector[], int low, int higt) {
-        if (low > higt)
-            return vector;
-
-        // Seleccion del elemento Pivote
-        int medio = low + (higt - low) / 2;
-        double pivote = vector[medio];
-        while (low <= higt) {
-            while (vector[low] < pivote) {
-                low++;
-            }
-            while (vector[higt] > pivote) {
-                higt--;
-            }
-            // hacer el swap
-            if (low <= higt) {
-                double temp = vector[low];
-                vector[low] = vector[higt];
-                vector[higt] = temp;
-                low++;
-                higt--;
-            }
-            // Volvemos a llamar al ordenamiento si aun sigue desordenado
-            if (low < higt)
-                return quicksort(vector, 0, vector.length-1);
-        }
-        return vector;
-    }
-
-    public static  void busquedabinaria(double vector[], double valor) {
-        double[] vect = sort(vector);// Recibimos el Vector Ordenado para Busqueda Binaria
-        int low_indice = 0;
-        int high_indice = vect.length - 1;
-
-        while (low_indice <= high_indice) {
-            int medio_indice = (low_indice + high_indice) / 2;
-            if (valor == vect[medio_indice]) {
-                break;
-            } else if (valor < vect[medio_indice]) {
-                high_indice = medio_indice - 1;
-            } else if (valor > vect[medio_indice]) {
-                low_indice = medio_indice + 1;
-            }
-        }
-    }
 }
->>>>>>> e3035f2 (no funciona fibbo)
