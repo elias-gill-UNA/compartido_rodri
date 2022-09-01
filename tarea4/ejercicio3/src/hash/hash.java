f735308c59c7b06b07305266d43eba7f8fcd548a
Elias Gill <eliasgill42@gmail.com>
2022-09-01 13:50:42 -0400

    comenzando tarea 4

--- tarea4/ejercicio3/src/hash/hash.java
+++ tarea4/ejercicio3/src/hash/hash.java
@@ -1,2 +1,12 @@
 package hash;
 
+public class hash {
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("hola que hace");
+		} else {
+			System.out.println("perderdor");
+		}
+	}
+}
